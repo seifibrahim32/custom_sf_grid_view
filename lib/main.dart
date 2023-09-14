d429662641006fbc3a59803614013f5ae9d55af3
seifibrahim32 <seifibrahim32@gmail.com>
2023-09-14 18:51:09 +0300

    Items filtered

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,59 @@
-import 'package:custom_sf_grid_view/models/employee_model.dart';
+
 import 'package:custom_sf_grid_view/src/datagrid_widget/runtime/column.dart';
-import 'package:custom_sf_grid_view/viewmodel/datasources/employee_local_source.dart';
 import 'package:flutter/material.dart';
 
 import 'package:custom_sf_grid_view/custom_sf_grid_view.dart';
+class EmployeeDataSource extends DataGridSource {
+  /// Creates the employee data source class with required details.
+  EmployeeDataSource({required List<EmployeeModel> employeeData}) {
+    _employeeData = employeeData
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+      DataGridCell<int>(columnName: 'id', value: e.id),
+      DataGridCell<String>(columnName: 'name', value: e.name),
+      DataGridCell<String>(
+          columnName: 'designation', value: e.designation),
+      DataGridCell<int>(columnName: 'salary', value: e.salary),
+    ]))
+        .toList();
+  }
 
-import 'custom_sf_grid_view.dart';
+  List<DataGridRow> _employeeData = [];
 
+  @override
+  List<DataGridRow> get rows => _employeeData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        color: Colors.greenAccent,
+        cells: row.getCells().map<Widget>((e) {
+          return Container(
+            color: Colors.blue,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(8.0),
+            child: Text(e.value.toString(),
+                style: const TextStyle(color: Colors.white)),
+          );
+        }).toList());
+  }
+}
+
+class EmployeeModel {
+  /// Creates the employee class with required details.
+  EmployeeModel(this.id, this.name, this.designation, this.salary);
+
+  /// Id of an employee.
+  final int id;
+
+  /// Name of an employee.
+  final String name;
+
+  /// Designation of an employee.
+  final String designation;
+
+  /// Salary of an employee.
+  final int salary;
+}
 void main() async {
   runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
